764de37fc82c6059b050c33bde5e2f632f8117e5
moruzerinho6 <zerinho6@gmail.com>
2020-05-10 21:36:00 -0300

    Fixed wrong image size on avatar.js

--- src/commands/utility/avatar.js
+++ src/commands/utility/avatar.js
@@ -1,6 +1,6 @@
 exports.run = ({ message, ArgsManager, i18n, Send, fastEmbed }) => {
   if (ArgsManager.Argument && ArgsManager.Argument[0].toLowerCase() === i18n.__('Avatar_server')) {
-    fastEmbed.setImage(message.guild.iconURL() ? message.guild.iconURL({ size: 2046 }) : 'https://discordapp.com/assets/dd4dbc0016779df1378e7812eabaa04d.png')
+    fastEmbed.setImage(message.guild.iconURL() ? message.guild.iconURL({ size: 2048 }) : 'https://discordapp.com/assets/dd4dbc0016779df1378e7812eabaa04d.png')
   } else {
     fastEmbed.setImage(message.mentions.users.first() ? message.mentions.users.first().displayAvatarURL({ dynamic: true }) : message.author.displayAvatarURL({ dynamic: true }))
   }
